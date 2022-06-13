af41c664699e06467cb5a2c3305c59f725bba619
Mouy-im <75069309+Mouy-im@users.noreply.github.com>
2022-06-13 22:01:57 +0200

    polish display lang

--- src/components/Nav/Lang.jsx
+++ src/components/Nav/Lang.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faChevronDown} from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 import { useTranslation, Trans } from 'react-i18next';
 
@@ -6,17 +9,25 @@ const lngs = {
   fr: { nativeName: 'French' }
 };
 
+
 const Lang = () => {
     const { i18n } = useTranslation();
- 
+    const [open, setOpen] = useState(false);
+    
     return (
-        <div className="lang">
+      
+        <div className="lang-menu" onClick={() => setOpen(!open)} >
+            <img className="current" width="30" src={`https://admin.hypnosolution-accompagnement.com/wp-content/uploads/2022/05/${i18n.resolvedLanguage}.jpg`} />
+            <FontAwesomeIcon className={ open ? 'show' :  'hide' } icon={ faChevronDown }/>
+            <div className={ "lang " + (open ? 'openMenu' : 'closeMenu')} open={open} >
                 {Object.keys(lngs).map((lng) => (
                     <div key={lng}>
-                        <img style={{ display: i18n.resolvedLanguage === lng ? 'none' : 'block' }} width="30" src={`https://admin.hypnosolution-accompagnement.com/wp-content/uploads/2022/05/${lng}.jpg`} onClick={() => i18n.changeLanguage(lng)} />
+                        <img style={{ display: i18n.resolvedLanguage === lng ? 'none' : 'block' }} width="30" src={`https://admin.hypnosolution-accompagnement.com/wp-content/uploads/2022/05/${lng}.jpg`} onClick={() => { setOpen(!open);i18n.changeLanguage(lng) }} />
                     </div>
                 ))}
+            </div>
         </div>
+      
     )
 }
  
