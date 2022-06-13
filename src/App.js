f2dd62bccf0b03732cbf25d6a1b2bb47244c9d6c
Mouy-im <75069309+Mouy-im@users.noreply.github.com>
2022-06-13 21:58:39 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import NotFound from './pages/NotFound';
 import Navbar from './components/Nav/Navbar';
 import { MemoryRouter } from 'react-router';
 import { useTranslation } from 'react-i18next';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
 
 function App() {
   
@@ -18,8 +20,10 @@ function App() {
             <Route exact path="/" element={<Home t={t} i18n={i18n} />} />
             <Route exact path="/articles" element={<Articles t={t} i18n={i18n} />} />
             <Route exact path="/article/:id/:slug" element={<Article t={t} i18n={i18n} />} />
+            <Route exact path="/contact" element={<Contact t={t} i18n={i18n} />} />
             <Route path="*" element={<NotFound t={t} i18n={i18n} />} />
           </Routes>
+          <Footer t={t} />
       </div>
     </BrowserRouter>
   );
