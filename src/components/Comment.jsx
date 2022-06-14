d4e28c767ea98f54e89eada17f17b2b59b9dd76c
Mouy-im <75069309+Mouy-im@users.noreply.github.com>
2022-06-14 10:10:16 +0200

    change display

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Comment = ({comment, t}) => {
+const Comment = ({comment, t, i18n}) => {
+    const dateParser = (date) =>{
+        let newDate = new Date(date).toLocaleDateString(i18n.language, {
+            year: "numeric",
+            month:"long",
+            day:"numeric",
+            hour:"numeric",
+            minute:"numeric",
+            second:"numeric",
+        });
+        return newDate;
+    }
     return (
         <div className='comment'>
-            <h3>{comment.id}</h3>
-            <p>{comment.author_name}</p>
-            <p>{comment.date}</p>
+            <h5>{t('Comment of')} {comment.author_name} {t('at')} {dateParser(comment.date)}</h5>
             <p dangerouslySetInnerHTML={{__html:comment.content.rendered}}></p>
         </div>
     );
